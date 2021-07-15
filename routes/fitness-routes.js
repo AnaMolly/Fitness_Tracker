a1f4c3a5e5a818f9ede3fbbf5c72e594b10b9ee8
AnaMolly <analia.mohamdally@gmail.com>
2021-07-14 20:48:46 -0400

    added more get routes

--- routes/fitness-routes.js
+++ routes/fitness-routes.js
@@ -11,16 +11,42 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+
 router.get("/api/workouts", (req, res) => {
-  Workout.find()
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+  Workout.aggregate([
+    {
+      $addFields: {
+          totalDuration: {
+              $sum: '$exercises.duration'
+          },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+        res.json(dbWorkout)
     })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+    .catch((err) => {
+        res.json(err)
+    })
 });
 
+router.get(`/api/workouts/range`, (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration:
+            { $sum: '$exercises.duration' }
+        }
+    }
+  ])
+    .limit(7)
+    .then((dbWorkout) => {
+        res.json(dbWorkout)
+    })
+    .catch((err) => {
+        res.json(err)
+    })
+});
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
